65b7cd16c779d89d13067bd22120f372e0ca9d23
dhghuy2909 <dhghuy2002@gmail.com>
2024-09-21 14:31:20 +0700

    Led Status

---
+++ denled/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Status from './components/Status';
+
+function App() {
+  return (
+    <div className="App">
+      <Status></Status>
+    </div>
+  );
+}
+
+export default App;
---
+++ denled/src/components/Status.jsx
@@ -0,0 +1,16 @@
+export default function Status() {
+    return (
+      <div id="container">
+        <div id="title">
+          <h1>ĐIỀU KHIỂN ĐÈN LED</h1>
+        </div>
+        <div id="status">
+          <h3>Led Status:</h3>
+        </div>
+        <div id="button">
+          <div id="top">ON</div>
+          <div id="bottom">OFF</div>
+        </div>
+      </div>
+    );
+  }
